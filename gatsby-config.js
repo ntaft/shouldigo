5fd0b41b07297899b60c6e100474ed70d5ad3999
Nick Taft <20643993+ntaft@users.noreply.github.com>
2020-03-12 15:48:50 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Should I Go?`,
+    description: `Handy app that answers all questions about whether or not to participate in activities during the coronavirus outbreak`,
+    author: `Nick Taft`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
